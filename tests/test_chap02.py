0dadf33ed42103331204b91b62ff5242bc6791e5
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2020-05-15 12:54:43 +0900

    fix typo

--- tests/test_chap02.py
+++ tests/test_chap02.py
@@ -37,49 +37,49 @@ class Chap02Test(unittest.TestCase):
 
     def test_save_two_lines(self):
         """
-        12. 1列目をcol1.txtに，2列目をcol2.txtに保存Permalink
+        12. 1列目をcol1.txtに，2列目をcol2.txtに保存
         各行の1列目だけを抜き出したものをcol1.txtに，2列目だけを抜き出したものをcol2.txtとしてファイルに保存せよ．確認にはcutコマンドを用いよ．
         """
         pass
 
     def test_merge_two_textfiles(self):
         """
-        13. col1.txtとcol2.txtをマージPermalink
+        13. col1.txtとcol2.txtをマージ
         12で作ったcol1.txtとcol2.txtを結合し，元のファイルの1列目と2列目をタブ区切りで並べたテキストファイルを作成せよ．確認にはpasteコマンドを用いよ．
         """
         pass
 
     def test_n_lines(self):
         """
-        14. 先頭からN行を出力Permalink
+        14. 先頭からN行を出力
         自然数Nをコマンドライン引数などの手段で受け取り，入力のうち先頭のN行だけを表示せよ．確認にはheadコマンドを用いよ．
         """
         pass
 
     def test_last_n_lines(self):
         """
-        15. 末尾のN行を出力Permalink
+        15. 末尾のN行を出力
         自然数Nをコマンドライン引数などの手段で受け取り，入力のうち末尾のN行だけを表示せよ．確認にはtailコマンドを用いよ．
         """
         pass
 
     def test_split_a_file_into_n_pieces(self):
         """
-        16. ファイルをN分割するPermalink
+        16. ファイルをN分割する
         自然数Nをコマンドライン引数などの手段で受け取り，入力のファイルを行単位でN分割せよ．同様の処理をsplitコマンドで実現せよ．
         """
         pass
 
     def test_distict_string_in_the_first_column(self):
         """
-        17. １列目の文字列の異なりPermalink
+        17. １列目の文字列の異なり
         1列目の文字列の種類（異なる文字列の集合）を求めよ．確認にはcut, sort, uniqコマンドを用いよ．
         """
         pass
 
     def test_sort_lines_in_asc_of_third_column(self):
         """
-        18. 各行を3コラム目の数値の降順にソートPermalink
+        18. 各行を3コラム目の数値の降順にソート
         各行を3コラム目の数値の逆順で整列せよ\
         （注意: 各行の内容は変更せずに並び替えよ）．確認にはsortコマンドを用いよ（この問題はコマンドで実行した時の結果と合わなくてもよい）．
         """
@@ -87,7 +87,7 @@ class Chap02Test(unittest.TestCase):
 
     def test_frequency_of_a_string_in_the_first_column_in_des(self):
         """
-        19. 各行の1コラム目の文字列の出現頻度を求め，出現頻度の高い順に並べるPermalink
+        19. 各行の1コラム目の文字列の出現頻度を求め，出現頻度の高い順に並べる
         各行の1列目の文字列の出現頻度を求め，その高い順に並べて表示せよ．確認にはcut, uniq, sortコマンドを用いよ．
         """
         pass
