729531d7a346a8a277afad255044864e2d90b0c1
卍diohabara卍 <diohabara@gmail.com>
2023-01-14 19:15:10 +0900

    wip

--- nlp100/chap03.py
+++ nlp100/chap03.py
@@ -10,9 +10,28 @@ def fun20() -> str:
 
 def fun21() -> str:
     data = fun20()
-    pattern = r"\[\[Category:.*\s*\]\]"
+    pattern = r"\[\[Category:.*\]\]"
     prog = re.compile(pattern)
     return "\n".join(prog.findall(data))
+
+
+def fun22() -> list[str]:
+    data = fun20()
+    pattern = r"\[\[Category:(.*)\]\]"
+    prog = re.compile(pattern)
+    return prog.findall(data)
+
+
+def fun23() -> list[tuple[str, int]]:
+    data = fun20()
+    res = []
+    level1_pattern = r"={2}(.*)={2}"
+    prog1 = re.compile(level1_pattern)
+    res.extend([(name, 1) for name in prog1.findall(data)])
+    level2_pattern = r"={3}(.*)={3}"
+    prog2 = re.compile(level2_pattern)
+    res.extend([(name, 2) for name in prog2.findall(data)])
+    return res
 
 
 if __name__ == "__main__":
--- tests/test_chap03.py
+++ tests/test_chap03.py
@@ -2,7 +2,7 @@
 Chapter 3: Regular Expression
 """
 
-from nlp100.chap03 import fun20, fun21
+from nlp100.chap03 import fun20, fun21, fun22, fun23
 
 
 def test_fun20() -> None:
@@ -43,12 +43,116 @@ def test_fun22() -> None:
     """
     Extract the category names of the article.
     """
+    expected = [
+        "United Kingdom| ",
+        "British Islands",
+        "Countries in Europe",
+        "English-speaking countries and territories",
+        "G7 nations",
+        "Group of Eight nations",
+        "G20 nations",
+        "Island countries",
+        "Northern European countries",
+        "Former member states of the European Union",
+        "Member states of NATO",
+        "Member states of the Commonwealth of Nations",
+        "Member states of the Council of Europe",
+        "Member states of the Union for the Mediterranean",
+        "Member states of the United Nations",
+        "Priority articles for attention after Brexit",
+        "Western European countries",
+    ]
+    achieved = fun22()
+    assert expected == achieved
 
 
 def test_fun23() -> None:
     """
     Extract section names in the article with their levels. For example, the level of the section is 1 for the MediaWiki markup "== Section name ==".
     """
+    expected = [
+        ("Etymology and terminology", 1),
+        ("History", 1),
+        ("=Background=", 1),
+        ("=Treaty of Union=", 1),
+        ("=From the union with Ireland to the end of the First World War=", 1),
+        ("=Between the World Wars=", 1),
+        ("=Since the Second World War=", 1),
+        ("Geography", 1),
+        ("=Climate=", 1),
+        ("=Administrative divisions=", 1),
+        ("Dependencies", 1),
+        ("Politics", 1),
+        ("=Government=", 1),
+        ("=Devolved administrations=", 1),
+        ("=Law and criminal justice=", 1),
+        ("=Foreign relations=", 1),
+        ("=Military=", 1),
+        ("Economy", 1),
+        ("=Overview=", 1),
+        ("=Science and technology=", 1),
+        ("=Transport=", 1),
+        ("=Energy=", 1),
+        ("=Water supply and sanitation=", 1),
+        ("Demographics", 1),
+        ("=Ethnic groups=", 1),
+        ("=Languages=", 1),
+        ("=Religion=", 1),
+        ("=Migration=", 1),
+        ("=Education=", 1),
+        ("=Health=", 1),
+        ("Culture", 1),
+        ("=Literature=", 1),
+        ("=Music=", 1),
+        ("=Visual art=", 1),
+        ("=Cinema=", 1),
+        ("= Cuisine =", 1),
+        ("=Media=", 1),
+        ("=Philosophy=", 1),
+        ("=Sport=", 1),
+        ("=Symbols=", 1),
+        ("=Stereotypes=", 1),
+        ("Historiography", 1),
+        ("See also", 1),
+        ("Notes", 1),
+        ("References", 1),
+        ("External links", 1),
+        ("Background", 2),
+        ("Treaty of Union", 2),
+        ("From the union with Ireland to the end of the First World War", 2),
+        ("Between the World Wars", 2),
+        ("Since the Second World War", 2),
+        ("Climate", 2),
+        ("Administrative divisions", 2),
+        ("Government", 2),
+        ("Devolved administrations", 2),
+        ("Law and criminal justice", 2),
+        ("Foreign relations", 2),
+        ("Military", 2),
+        ("Overview", 2),
+        ("Science and technology", 2),
+        ("Transport", 2),
+        ("Energy", 2),
+        ("Water supply and sanitation", 2),
+        ("Ethnic groups", 2),
+        ("Languages", 2),
+        ("Religion", 2),
+        ("Migration", 2),
+        ("Education", 2),
+        ("Health", 2),
+        ("Literature", 2),
+        ("Music", 2),
+        ("Visual art", 2),
+        ("Cinema", 2),
+        (" Cuisine ", 2),
+        ("Media", 2),
+        ("Philosophy", 2),
+        ("Sport", 2),
+        ("Symbols", 2),
+        ("Stereotypes", 2),
+    ]
+    achieved = fun23()
+    assert expected == achieved
 
 
 def test_fun24() -> None:
